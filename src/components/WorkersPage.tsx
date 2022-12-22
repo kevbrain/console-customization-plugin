208fbb5931100e41a517b5d84ded240a44c2d7ac
'ksc' <'ksc@example.com';>
2022-12-22 15:43:42 +0100

    retry

--- src/components/WorkersPage.tsx
+++ src/components/WorkersPage.tsx
@@ -1,16 +1,15 @@
 import * as React from "react";
-import {  useEffect } from "react";
+import { useState, useEffect } from 'react';
 
-
-//import {Worker} from "../../app/models";
-//import {WorkersTable }from "../../app/Components/Tables";
+import {Worker} from "../app/models";
+import {WorkersTable }from "../app/Components/Tables";
 export const WorkersPage = () => {
-  //const [workers, setWorkers] = useState<Worker[]>([]);
+  const [workers, setWorkers] = useState<Worker[]>([]);
   async function fetchWorkersHandler() {
     const response = await fetch('https://capacity-tool-route-capacity-tool-dev.apps.ocp-lab2.its4u.eu/api/v1/nodes');
     console.log('in workerPage after fetch nodes ' + response + ' where response status is ' + response.status);
     const workersData = await response.json();
-    //setWorkers(workersData);
+    setWorkers(workersData);
     console.log('in workerPage after setWorkersData where workersData ' + workersData);
   }
  
@@ -19,12 +18,11 @@ export const WorkersPage = () => {
     
   }, []);
   let content = <React.Fragment />;
-  //if (workers.length > 0) {
-  //  content = <WorkersTable workers={workers} />;
-  //}
+  if (workers.length > 0) {
+    content = <WorkersTable workers={workers} />;
+  }
   return (
     <React.Fragment>
-      <h1>WORKERS</h1>
       {content}
     </React.Fragment>
   );
