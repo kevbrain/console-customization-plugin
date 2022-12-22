5c8b4c7f29e747e57f63aa9ae2b3831c34d73592
'ksc' <'ksc@example.com';>
2022-12-22 14:13:02 +0100

    refractor without totals

--- src/app/pages/WorkersPage.tsx
+++ src/app/pages/WorkersPage.tsx
@@ -2,13 +2,10 @@ import * as React from "react";
 import { useState, useEffect } from "react";
 
 
-import {Worker,Totals} from "../../app/models";
+import {Worker} from "../../app/models";
 import {WorkersTable }from "../../app/Components/Tables";
-
-
 export const WorkersPage = () => {
   const [workers, setWorkers] = useState<Worker[]>([]);
-  const [totals, setTotals] = useState<Totals>(new Totals());
   async function fetchWorkersHandler() {
     const response = await fetch('https://capacity-tool-route-capacity-tool-dev.apps.ocp-lab2.its4u.eu/api/v1/nodes');
     console.log('in workerPage after fetch nodes ' + response + ' where response status is ' + response.status);
@@ -16,22 +13,14 @@ export const WorkersPage = () => {
     setWorkers(workersData);
     console.log('in workerPage after setWorkersData where workersData ' + workersData);
   }
-  async function fetchClustersHandler() {
-    const response = await fetch(
-      ' https://capacity-tool-route-capacity-tool-dev.apps.ocp-lab2.its4u.eu/api/v1/cluster'
-    );
-    console.log('in workerPage after fetch clusters ' + response + ' where response status is ' + response.status);
-    const totalsData = await response.json();
-    setTotals(totalsData);
-    console.log('in workerPage after setTotalsData where totalsData ' + totalsData);
-  }
+ 
   useEffect(() => {
     fetchWorkersHandler();
-    fetchClustersHandler();
+    
   }, []);
   let content = <React.Fragment />;
-  if (workers.length > 0 && totals != null) {
-    content = <WorkersTable workers={workers} totals={totals} />;
+  if (workers.length > 0) {
+    content = <WorkersTable workers={workers} />;
   }
   return (
     <React.Fragment>
@@ -39,4 +28,7 @@ export const WorkersPage = () => {
       {content}
     </React.Fragment>
   );
-};export default WorkersPage;
+};
+
+
+export default WorkersPage;
