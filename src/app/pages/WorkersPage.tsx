5e8c504d5f5bcd9254dfecd462349edbdc2079e2
'ksc' <'ksc@example.com';>
2022-12-22 14:55:17 +0100

    fx

--- src/app/pages/WorkersPage.tsx
+++ src/app/pages/WorkersPage.tsx
@@ -1,16 +1,16 @@
 import * as React from "react";
-import { useState, useEffect } from "react";
+import {  useEffect } from "react";
 
 
-import {Worker} from "../../app/models";
+//import {Worker} from "../../app/models";
 //import {WorkersTable }from "../../app/Components/Tables";
 export const WorkersPage = () => {
-  const [setWorkers] = useState<Worker[]>([]);
+  //const [workers, setWorkers] = useState<Worker[]>([]);
   async function fetchWorkersHandler() {
     const response = await fetch('https://capacity-tool-route-capacity-tool-dev.apps.ocp-lab2.its4u.eu/api/v1/nodes');
     console.log('in workerPage after fetch nodes ' + response + ' where response status is ' + response.status);
     const workersData = await response.json();
-    setWorkers(workersData);
+    //setWorkers(workersData);
     console.log('in workerPage after setWorkersData where workersData ' + workersData);
   }
  
