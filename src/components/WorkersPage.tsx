18235ec811c0481800ca35503223577564471e71
'ksc' <'ksc@example.com';>
2022-12-23 11:51:14 +0100

    fix

--- src/components/WorkersPage.tsx
+++ src/components/WorkersPage.tsx
@@ -8,7 +8,7 @@ import { Button } from '@patternfly/react-core';
 export const WorkersPage = () => {
   const [workers, setWorkers] = useState<Worker[]>([]);
   async function fetchWorkersHandler() {
-    const response = await fetch('capacity-tool-service.capacity-tool-dev.svc.cluster.local/api/v1/nodes:8080');
+    const response = await fetch('capacity-tool-service.capacity-tool-dev.svc.cluster.local:8080/api/v1/nodes');
     console.log('in workerPage after fetch nodes ' + response + ' where response status is ' + response.status);
     const workersData = await response.json();
     setWorkers(workersData);
